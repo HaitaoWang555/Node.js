29f49bff3a2c37197d4976a003a7a879beaad6ba
wanghaitao <15124505701@163.com>
2018-05-31 17:13:07 +0800

    add eslint-airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "extends": "airbnb-base",
+  rules: {
+    "no-alert": "off",
+    'no-console': 'off',
+  },
+};
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-console.log('Hello,World');
+const A = 0;
+console.log(`Hello,World ${A}`);
